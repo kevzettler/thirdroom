76352f759f43d50010cece5f6d28b2edcc133744
Kev Zettler <kevzettler@gmail.com>
2022-11-19 19:09:46 -0800

    Clean up OfflineView components

--- src/ui/views/OfflineView.tsx
+++ src/ui/views/OfflineView.tsx
@@ -6,12 +6,15 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 import "./session/SessionView.css";
 import { useInitMainThreadContext, MainThreadContextProvider, useMainThreadContext } from "../hooks/useMainThread";
 import { useEvent } from "../hooks/useEvent";
+import { useKeyDown } from "../hooks/useKeyDown";
 import { usePointerLockChange } from "../hooks/usePointerLockChange";
 import { LoadingScreen } from "./components/loading-screen/LoadingScreen";
 import { loadWorld, enterWorld } from "../../plugins/thirdroom/thirdroom.main";
 
-function InputWrapper() {
+function OfflineContent() {
   const [_isPointerLock, setIsPointerLock] = useState(false);
+  const [_editorEnabled, setEditorEnabled] = useState(false);
+  const [_statsEnabled, setStatsEnabled] = useState(false);
   const mainThread = useMainThreadContext();
   usePointerLockChange(mainThread.canvas, setIsPointerLock, []);
 
@@ -24,12 +27,20 @@ function InputWrapper() {
     []
   );
 
-  return null;
-}
-
-export default function OfflineView() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const mainThread = useInitMainThreadContext(canvasRef);
+  useKeyDown(
+    (e) => {
+      if (e.code === "Backquote") {
+        setEditorEnabled((enabled) => !enabled);
+      }
+      if (e.code === "KeyS" && e.shiftKey && e.ctrlKey) {
+        setStatsEnabled((enabled) => !enabled);
+      }
+      if (e.code === "KeyP") {
+        document.exitPointerLock();
+      }
+    },
+    []
+  );
 
   async function initWorld() {
     if (!mainThread) return null;
@@ -47,15 +58,20 @@ export default function OfflineView() {
   }, [mainThread]);
 
 
+  return null;
+}
+
+export default function OfflineView() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const mainThread = useInitMainThreadContext(canvasRef);
+
   return (
     <DndProvider backend={HTML5Backend}>
       <div className="SessionView">
         <canvas className="SessionView__viewport" ref={canvasRef} />
         {mainThread ? (
           <MainThreadContextProvider value={mainThread}>
-            <Outlet />
-            <InputWrapper />
-            {/* <StatusBar /> */}
+            <OfflineContent />
           </MainThreadContextProvider>
         ) : (
           <LoadingScreen message="Initializing engine..." />
